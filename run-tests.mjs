ce08c3175ce82dceb89d08cf219645c236c0a223
Eric Holk <eholk@google.com>
2017-11-20 12:43:44 -0800

    Convert to ES6 modules in anticipation of adding runtime calls

--- run-tests.mjs
+++ run-tests.mjs
@@ -1,9 +1,10 @@
-#!/usr/bin/env node
+// -*- javascript-mode -*-
+import rt from './rt/rt';
 
-const assert = require('assert');
-const child_process = require('child_process');
-const fs = require('fs');
-const util = require('util')
+import assert from 'assert';
+import child_process from 'child_process';
+import fs from 'fs';
+import util from 'util';
 
 const TAG_SIZE = 3;
 const TAGS = {
@@ -39,28 +40,28 @@ function js_from_scheme(ptr) {
 }
 
 async function runTest(name) {
-    const { stdout, stderr } = await util.promisify(child_process.exec)(`./schism.ss ${name}`);
-    
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-    
-    const file = fs.readFileSync('out.wasm');
-    const wasm = new WebAssembly.Module(file);
+  const { stdout, stderr } = await util.promisify(child_process.exec)(`./schism.ss ${name}`);
 
-    const instance = await WebAssembly.instantiate(wasm);
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
 
-    const result = js_from_scheme(instance.exports['do-test']());
-    
-    assert.equal(result == false || result == 1, 1, "test failed");
+  const file = fs.readFileSync('out.wasm');
+  const wasm = new WebAssembly.Module(file);
+
+  const instance = await WebAssembly.instantiate(wasm, { 'rt': rt });
+
+  const result = js_from_scheme(instance.exports['do-test']());
+
+  assert.equal(result == false || result == 1, 1, "test failed");
 }
 
 async function runTests() {
     const files = await util.promisify(fs.readdir)('test');
     for (const test of files) {
-	if (test.endsWith(".ss")) {
-	    console.info(`Running test ${test}`);
-	    await runTest(`test/${test}`);
-	}
+		if (test.endsWith(".ss")) {
+			console.info(`Running test ${test}`);
+			await runTest(`test/${test}`);
+		}
     }
 }
 
