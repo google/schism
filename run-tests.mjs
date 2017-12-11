7accf054ea500504560ede20c76efa335fb7b6f0
Eric Holk <eholk@google.com>
2017-12-11 12:06:43 -0800

    More robust resting framework

--- run-tests.mjs
+++ run-tests.mjs
@@ -6,12 +6,23 @@ import child_process from 'child_process';
 import fs from 'fs';
 import util from 'util';
 
+const OPTIONS = {
+    use_snapshot: true, // load schism-stage0.wasm if true instead of
+                        // building with host scheme
+
+    // which stages to build and run tests for
+    stage0: true,
+    stage1: false,
+    stage2: false,
+    stage3: false, // compile-only, no point running tests
+}
+
 // Returns the contents of out.wasm
 async function compileWithHostScheme(name) {
     const { stdout, stderr } = await util.promisify(child_process.exec)(`./schism.ss ${name}`);
 
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
+    //console.log(`stdout: ${stdout}`);
+    //console.log(`stderr: ${stderr}`);
 
     return util.promisify(fs.readFile)('out.wasm');
 }
@@ -46,14 +57,38 @@ async function runTest(name, compile = compileWithHostScheme) {
     assert.ok(result != false, "test failed");
 }
 
-async function runTests(compile = compileWithHostScheme) {
+async function runTests() {
+    let failures = [];
     const files = await util.promisify(fs.readdir)('test');
     for (const test of files) {
 	if (test.endsWith(".ss")) {
+	    let local_failures = [];
 	    console.info(`Running test ${test}`);
-	    await runTest(`test/${test}`, compile);
+	    async function run_stage(name, compile) {
+		try {
+		    await runTest(`test/${test}`, compile);
+		    console.info(`  ${name} succeeded`);
+		} catch (e) {
+		    console.info(`  ${name} FAILED`);
+		    console.info(e.stack);
+		    local_failures.push([name]);
+		}
+	    }
+	    if (OPTIONS.stage0) {
+		await run_stage("stage0", stage0_compile);
+	    }
+	    if (OPTIONS.stage1) {
+		await run_stage("stage1", stage1_compile);
+	    }
+	    if (OPTIONS.stage2) {
+		await run_stage("stage2", stage2_compile);
+	    }
+	    if (local_failures.length > 0) {
+		failures.push([test, local_failures]);
+	    }
 	}
     }
+    return failures;
 }
 
 async function createSchismFromWasm(schism_bytes) {
@@ -70,23 +105,54 @@ async function createSchismFromWasm(schism_bytes) {
     };
 }
 
-const use_stage0 = false;
-const compileWithWasmScheme = (async function() {
-  const schism_bytes = use_stage0 ? fs.readFileSync('schism-stage0.wasm') : await compileBootstrap();
-  return createSchismFromWasm(schism_bytes);
-}());
+function make_compiler(compiler_bytes) {
+    return async function(bytes) {
+	const engine = new Schism.Engine;
+	const schism = await engine.loadWasmModule(await compiler_bytes);
+	engine.setCurrentInputPort(bytes);
+	schism.exports['compile-stdin->stdout']();
+	return new Uint8Array(engine.output_data);
+    }
+}
 
-const use_host_compiler = false;
-if (use_host_compiler) {
-    runTests().catch((e) => {
-      console.error(e.stack);
-      throw e;
+// The stage0 bytes, either loaded from a snapshot
+// (schism-stage0.wasm) or compiled by the host Scheme.
+const stage0_bytes = OPTIONS.stage0 ? (async function() {
+    return OPTIONS.use_snapshot
+	? fs.readFileSync('schism-stage0.wasm')
+	: await compileBootstrap()
+})() : undefined;
+// Compile bytes using the stage0 compiler.
+const stage0_compile = OPTIONS.stage0 ? make_compiler(stage0_bytes) : undefined;
+const stage1_bytes = OPTIONS.stage1 ? stage0_compile(fs.readFileSync('./schism/compiler.ss'))
+                                    : undefined;
+const stage1_compile = OPTIONS.stage1 ? make_compiler(stage1_bytes) : undefined;
+const stage2_bytes = OPTIONS.stage2 ? stage1_compile(fs.readFileSync('./schism/compiler.ss'))
+                                    : undefined;
+const stage2_compile = OPTIONS.stage2 ? make_compiler(stage2_bytes) : undefined;
+const stage3_bytes = OPTIONS.stage3 ? stage2_compile(fs.readFileSync('./schism/compiler.ss'))
+                                    : undefined;
+
+// We should be at a fixpoint by now, so we compile stage3 only to check for equality.
+async function checkCompilerFixpoint() {
+    assert.equal(await stage2_bytes, await stage3_bytes);
+}
+
+try {
+    let results = runTests().catch((e) => { throw e });
+    if (OPTIONS.stage3) {
+	checkCompilerFixpoint().catch((e) => { throw e });
+    }
+    results.then((failures) => {
+	if (failures.length > 0) {
+	    console.info("Some tests failed:");
+	    for(const failure of failures) {
+		console.info(`    '${failure[0]}': ${failure[1].join(', ')}`);
+	    }
+	    throw new Error("Tests failed");
+	}
     });
-} else {
-  compileWithWasmScheme.then((compile) => {
-    runTests(compile).catch((e) => {
-      console.error(e.stack);
-      throw e;
-    });
-  });
+} catch (e) {
+    console.error(e.stack);
+    throw e;
 }
