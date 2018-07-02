507d400a3c6f260433d6b21b09e2e64af8c96b3b
Matthew Phillips <phillipsm2@gmail.com>
2018-07-02 18:24:13 -0400

    Make stage-0 the default compile
    This also adds the `--stage` flag to specify which stage to use. Useful
    when developing schism itself.

--- run-schism.mjs
+++ run-schism.mjs
@@ -25,8 +25,20 @@ import cli from './cli';
 async function runSchism() {
     // set up the input port
     const input_file = cli.input[0] || "./schism/compiler.ss";
-    const compiler_output = await stage1_compile(fs.readFileSync(input_file));
+    const compiler_output = await getCompile()(fs.readFileSync(input_file));
     fs.writeFileSync(cli.flags.out, compiler_output);
+}
+
+function getCompile() {
+    switch(cli.flags.stage) {
+        case "1":
+            return stage1_compile;
+        case "2":
+            return stage2_compile;
+        case "0":
+        default:
+            return stage0_compile;
+    }
 }
 
 runSchism().catch((e) => {
